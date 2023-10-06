924d76e7d8d6fde422a23872c9ad91e85ffdf787
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-10-06 10:51:17 +0500

    init weather

--- script.js
+++ script.js
@@ -7,24 +7,34 @@ const options = {
     }
 };
 
-fetch(url, options)
-    .then(response => response.json())
-    .then(response => {
+const getWeather = (city) => {
+    cityName.innerHTML = city
+    fetch(url, options)
+        .then(response => response.json())
+        .then(response => {
 
-        console.log(response)
+            console.log(response)
 
-        cloud_pct.innerHTML = response.cloud_pct
-        temp.innerHTML = response.temp
-        feels_like.innerHTML = response.feels_like
-        humidity.innerHTML = response.humidity
-        min_temp.innerHTML = response.min_temp
-        max_temp.innerHTML = response.max_temp
-        wind_speed.innerHTML = response.wind_speed
-        wind_degrees.innerHTML = response.wind_degrees
-        sunrise.innerHTML = response.sunrise
-        sunset.innerHTML = response.sunset
-    })
-    .catch(err => console.error(err));
+            cloud_pct.innerHTML = response.cloud_pct
+            temp.innerHTML = response.temp
+            feels_like.innerHTML = response.feels_like
+            humidity.innerHTML = response.humidity
+            min_temp.innerHTML = response.min_temp
+            max_temp.innerHTML = response.max_temp
+            wind_speed.innerHTML = response.wind_speed
+            wind_degrees.innerHTML = response.wind_degrees
+            sunrise.innerHTML = response.sunrise
+            sunset.innerHTML = response.sunset
+        })
+        .catch(err => console.error(err));
+}
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    getWeather(city.value);
+})
+
+getWeather('Sukkur')
 
 
 
